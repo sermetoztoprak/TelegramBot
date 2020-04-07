6b6105952941b57f8e0b599f713e0715abba6dc6
sermet öztoprak <sermetoztoprak@gmail.com>
2020-04-07 13:10:30 +0300

    Lesson link added

--- index.js
+++ index.js
@@ -10,37 +10,125 @@ bot.onText(/\/start/, (msg, match) => {
         reply_to_message_id: msg.message_id,
         reply_markup: JSON.stringify({
             keyboard: [
-                ['Tarih'],
-                ['Coğrafya'],
-                ['Vatandaşlık'],
-                ['Türkçe'],
-                ['Matematik']
+                ['BenimHocam'],
+                ['Data'],
+                ['Lider'],
+                ['Murat'],
+                ['Pegem'],
+                ['Süvari'],
+                ['UzmanKariyer'],
+                ['Yargı'],
+                ['Yediiklim'],
+                ['Zorlu'],
+                ['İsem'],
+                ['MehmetEgit'],
+                ['EgitimBilimleri']
             ]
         })
     };
-    bot.sendMessage(msg.chat.id, 'Merhaba ben bir botum sana nasıl yardımcı olabilirim', opts);
+    bot.sendMessage(msg.chat.id, 'Merhaba hoşgeldiniz ben bir botum. 2020 denemeleri için aşağıdaki butonlara tıklayabilirsin.', opts);
 });
-bot.onText(/Tarih/, (msg, match) => {
-    // const opts = {
-    //     reply_markup: {
-    //         inline_keyboard: [
-    //             [{
-    //                     text: 'EUR',
-    //                     callback_data: JSON.stringify({
-    //                         'command': 'price',
-    //                         'base': 'EUR'
-    //                     })
-    //                 },
-    //                 {
-    //                     text: 'USD',
-    //                     callback_data: JSON.stringify({
-    //                         'command': 'price',
-    //                         'base': 'USD'
-    //                     })
-    //                 }
-    //             ]
-    //         ]
-    //     }
-    // }; 
-    bot.sendMessage(msg.chat.id, 'www.aradigimbilgi.com', opts);
+bot.onText(/BenimHocam/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/UHrW', opts);
+});
+bot.onText(/Data/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/C2wxa', opts);
+});
+bot.onText(/Lider/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/fyq7R0', opts);
+});
+bot.onText(/Murat/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/drDWM', opts);
+});
+bot.onText(/Pegem/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/VvKSgi', opts);
+});
+bot.onText(/Süvari/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/EgU', opts);
+});
+bot.onText(/UzmanKariyer/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/V3VwoY', opts);
+});
+bot.onText(/Yargı/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'http://tr.link/MveRJj', opts);
+});
+bot.onText(/Yediiklim/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/nGlJMY', opts);
+});
+bot.onText(/Zorlu/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/LLA2', opts);
+});
+bot.onText(/İsem/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'http://tr.link/kzd2', opts);
+});
+bot.onText(/MehmetEgit/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/zVuJNE', opts);
+});
+bot.onText(/EgitimBilimleri/, (msg, match) => {
+    const opts = {
+        reply_markup: {
+            
+        }
+    };
+    bot.sendMessage(msg.chat.id, 'https://tr.link/mhOTi', opts);
 });
